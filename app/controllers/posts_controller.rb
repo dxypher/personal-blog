1f1ef1fdb09d7e53750bef059bc9e04d6e99a25c
Nikhil <hira1sem@gmail.com>
2012-07-12 16:09:31 -0500

    Add Posts New action and view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,23 @@ class PostsController < ApplicationController
 		# @posts = @posts.page(params[:page]).per(4)
 	end
 
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.create(params[:post])
+		redirect_to :index
+	end
+
+	def edit
+		@post = Post.find_by_id(:id)
+	end
+
+	def update
+		
+	end
+
 	def show
 		@post = Post.find_by_id(params[:id])
 	end
